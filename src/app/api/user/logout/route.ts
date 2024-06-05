d2649824a41fde3ba559feb706353472c7a8a5f6
Moin3 <moinislam667@gmail.com>
2024-06-05 10:21:08 +0600

    cookies delete

--- src/app/api/user/logout/route.ts
+++ src/app/api/user/logout/route.ts
@@ -1,16 +1,24 @@
+// 'use server'
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from 'next/headers'
 
 export async function GET(request:NextRequest) {
   const response = NextResponse.json({
     message: "Logged out !!",
     success: true,
+    
+    
   });
+  cookies().set('token', '')
 
-  response.cookies.set("token", "", {
-      expires: new Date(0),
-      domain: 'fullstack-nextjs-blood-bank.vercel.app'
-  });
+  // response.cookies.set("token", "", {
+  //     expires: new Date(0),
+  //     domain: 'fullstack-nextjs-blood-bank.vercel.app'
+  // });
+
+  
 
   return response;
 }
+
 
