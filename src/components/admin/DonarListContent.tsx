78cf1c102a5c36dd97f72b055f11c4c5be939c3e
Moin3 <moinislam667@gmail.com>
2024-06-08 15:16:35 +0600

    dkdk

--- src/components/admin/DonarListContent.tsx
+++ src/components/admin/DonarListContent.tsx
@@ -1,118 +1,105 @@
 'use client'
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import moment from 'moment';
 import toast from 'react-hot-toast';
 
-
 const DonarListContent = () => {
-    const [isLoading, setIsLoading] = useState<boolean>();
-    const [recordedData,setRecordedData]=useState([] as any)
-    // const [loading, setLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [recordedData, setRecordedData] = useState([] as any);
+    const [loading, setLoading] = useState(false);
     const [deletingItemId, setDeletingItemId] = useState(null);
 
-
-    const getDonarRecord=async ()=>{
-
-         const response= await fetch('/api/inventory/donar-list') 
-            .then(response => {
-                if (!response.ok) {
-                console.log('Network response was not ok');
-                setIsLoading(false); 
-                return;
-                }
-                return response.json(); 
-                })
-            .then(data => {
-                setRecordedData((data as any).donarData)
-                setIsLoading(false); 
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-              });
-    }
-
-
-    useEffect(()=>{
-        getDonarRecord()
-    },[recordedData])
-
-
-    const handleDelete = async (id:any) => {
+    const getDonarRecord = async () => {
+        setIsLoading(true);
         try {
-            setDeletingItemId(id)
-            setIsLoading(true)
-            const response = await fetch(
-                '/api/user/delete', 
-                {
-                    method: 'DELETE',
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify(id),
-                }
-            )
-    
+            const response = await fetch('/api/inventory/donar-list');
             if (!response.ok) {
-                toast.error('Delete request failed')
-                setIsLoading(false)
+                console.log('Network response was not ok');
+                setIsLoading(false);
                 return;
-              }
-              const data = await response.json();
-              toast.success(data.message)
-              setIsLoading(false);
-              // window.location.reload()
-
-
-    
+            }
+            const data = await response.json();
+            setRecordedData(data.donarData);
         } catch (error) {
-            console.log('Error:', error);
-            setIsLoading(false); 
+            console.error('There was a problem with the fetch operation:', error);
+        } finally {
+            setIsLoading(false);
         }
     };
 
+    useEffect(() => {
+        getDonarRecord();
+    }, []);
 
+    const handleDelete = async (id: any) => {
+        try {
+            setDeletingItemId(id);
+            setLoading(true);
+            const response = await fetch(
+                '/api/user/delete',
+                {
+                    method: 'DELETE',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(id),
+                }
+            );
 
-  return (
-    <div className="w-[100vw] lg:w-[65vw] overflow-x-scroll  md:overflow-x-auto p-8">
-        {isLoading ? ( 
-        <div className=' min-h-[300px] flex justify-center items-center font-bold text-red-500'>Loading...</div>
-        ) : (
-            <table className="min-w-full bg-white border rounded-lg overflow-hidden">
-        <thead className="bg-gray-200">
-          <tr className="text-gray-700">
-            <th scope="col" className="px-6 py-3 text-left font-bold">Donar Name</th>
-            <th scope="col" className="px-6 py-3 text-left font-bold">Email</th>
-            <th scope="col" className="px-6 py-3 text-left font-bold">Phone</th>
-            <th scope="col" className="px-6 py-3 text-left font-bold">Date</th>
-            <th scope="col" className="px-6 py-3 text-left font-bold">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {recordedData?.map((item:any) => (
-            <tr key={item._id} className="hover:bg-gray-100">
-              <td className="p-3">{item.name}</td>
-              <td className="p-3">{item.email}</td>
-              <td className="p-3">{item.phone}</td>
-              <td className="p-3 text-sm">{moment(item.createdAt).format('DD/MM/YYYY hh:mm A')}</td>
-              <td className="p-3">
-              {deletingItemId === item._id ? (
-                'Deleting...'
-              ) : (
-                <span
-                  className="cursor-pointer text-blue-500 z-5"
-                  onClick={() => handleDelete(item._id)}
-                >
-                  Delete
-                </span>
-              )}
-            </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            if (!response.ok) {
+                toast.error('Delete request failed');
+                setLoading(false);
+                return;
+            }
+            const data = await response.json();
+            toast.success(data.message);
+            setRecordedData(recordedData.filter((item: any) => item._id !== id));
+        } catch (error) {
+            console.log('Error:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div className="w-[100vw] lg:w-[65vw] overflow-x-scroll md:overflow-x-auto p-8">
+            {isLoading ? (
+                <div className='min-h-[300px] flex justify-center items-center font-bold text-red-500'>Loading...</div>
+            ) : (
+                <table className="min-w-full bg-white border rounded-lg overflow-hidden">
+                    <thead className="bg-gray-200">
+                        <tr className="text-gray-700">
+                            <th scope="col" className="px-6 py-3 text-left font-bold">Donar Name</th>
+                            <th scope="col" className="px-6 py-3 text-left font-bold">Email</th>
+                            <th scope="col" className="px-6 py-3 text-left font-bold">Phone</th>
+                            <th scope="col" className="px-6 py-3 text-left font-bold">Date</th>
+                            <th scope="col" className="px-6 py-3 text-left font-bold">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {recordedData?.map((item: any) => (
+                            <tr key={item._id} className="hover:bg-gray-100">
+                                <td className="p-3">{item.name}</td>
+                                <td className="p-3">{item.email}</td>
+                                <td className="p-3">{item.phone}</td>
+                                <td className="p-3 text-sm">{moment(item.createdAt).format('DD/MM/YYYY hh:mm A')}</td>
+                                <td className="p-3">
+                                    {deletingItemId === item._id ? (
+                                        'Deleting...'
+                                    ) : (
+                                        <span
+                                            className="cursor-pointer text-blue-500 z-5"
+                                            onClick={() => handleDelete(item._id)}
+                                        >
+                                            Delete
+                                        </span>
+                                    )}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             )}
-    </div>
-  )
+        </div>
+    );
 }
 
-export default DonarListContent
-
-
+export default DonarListContent;
